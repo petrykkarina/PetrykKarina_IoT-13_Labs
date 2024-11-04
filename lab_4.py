6443b0ae9e1c4dcc5f9c91e247de33b48ba0f655
petrykkarina <karina.petryk.ir.2024@lpnu.ua>
2024-11-04 11:22:49 +0200

    lab_4.py

--- lab_4.py
+++ lab_4.py
@@ -1,65 +1,47 @@
 class ArtificialTree:
-    def __init__(self, manufacturer='', height=0, price=0.0, material='', public_num_field=0, public_str_field=''):
+    def __init__(self, manufacturer="", height=0, price=0.0, material=""):
        
         self.__manufacturer = manufacturer
         self.__height = height
         self.__price = price
         self.__material = material
 
-        
-        self.public_num_field = public_num_field  
-        self.public_str_field = public_str_field  
+       
+        self.number_field = 0
+        self.string_field = "Default"
 
-    
-    def __init__(self):
-        self.__manufacturer = "Невідомий"
-        self.__height = 0
-        self.__price = 0.0
-        self.__material = "Невідомий"
-        self.public_num_field = 0
-        self.public_str_field = "Невідомий"
-
-    
-    def __init__(self, manufacturer='', height=0, price=0.0, material='', public_num_field=0, public_str_field=''):
-        self.__manufacturer = manufacturer
-        self.__height = height
-        self.__price = price
-        self.__material = material
-        self.public_num_field = public_num_field  
-        self.public_str_field = public_str_field  
-
-    
-    def __del__(self):
-        print(f'Об\'єкт {self.__manufacturer} видалено')
-
-    
+   
     def get_manufacturer(self):
         return self.__manufacturer
+
+    def set_manufacturer(self, manufacturer):
+        self.__manufacturer = manufacturer
 
     def get_height(self):
         return self.__height
 
+    def set_height(self, height):
+        self.__height = height
+
     def get_price(self):
         return self.__price
+
+    def set_price(self, price):
+        self.__price = price
 
     def get_material(self):
         return self.__material
 
+    def set_material(self, material):
+        self.__material = material
+
     
     def __str__(self):
-        return f'Штучна ялинка: {self.__manufacturer}, Висота: {self.__height} см, Ціна: {self.__price} грн, Матеріал: {self.__material}'
+        return f"ArtificialTree(manufacturer={self.__manufacturer}, height={self.__height}, price={self.__price}, material={self.__material})"
 
     def __repr__(self):
-        return f'ArtificialTree(manufacturer={self.__manufacturer}, height={self.__height}, price={self.__price}, material={self.__material})'
-
-
-
-if __name__ == "__main__":
-   
-    tree1 = ArtificialTree("Ялинка Плюс", 180, 1200.5, "ПВХ", 5, "Декор")
-    tree2 = ArtificialTree("Еко Ялинка", 150, 950.0, "Пластик", 3, "Зелений")
-    tree3 = ArtificialTree("Лісова красуня", 200, 1500.0, "Сосна", 8, "Природний")
+        return self.__str__()
 
     
-    for tree in (tree1, tree2, tree3):
-        print(tree)
+    def __del__(self):
+        print(f"ArtificialTree {self.__manufacturer} is being deleted")
